acc934f9a910bbbefcc6c7e79da7813d4b9331af
haticeaydinn <48706428+haticeaydinn@users.noreply.github.com>
2020-02-09 01:58:01 +0300

    Strings
    Strings examples in Python

---
+++ app.py
@@ -0,0 +1,25 @@
+# exercises from Mosh
+"""
+print("Hatice Aydın")
+name = input("Write your name: ")
+print("Hi " + name)
+"""
+
+# calculate weight
+"""
+weight_lbs = input("Write your weight in lbs: ")
+weight_kg = float(weight_lbs) * 0.45
+print(type(weight_kg))
+print(weight_kg)
+"""
+
+# Strings
+message = '''Hi Hatice,
+This is your first mail from Python.
+Best Regards.'''
+print(message)
+print(message[-4])  # indexing a string, reverse
+print(message[0:4])
+print(message[:3])
+print(message[:])  # copy string
+
